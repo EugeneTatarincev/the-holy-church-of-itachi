6484dedd1b64759113b8b9592233e0a835cfe4c7
Eugene <tatr12345@bk.ru>
2020-05-30 14:33:28 +0300

    mangek

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const audio = document.querySelector("audio"),
-        test = document.getElementById('test');
+        test = document.getElementById('test'),
+        audio2 = document.querySelector("#audio2");
 
 // ADJUSTING AUDIO
 
@@ -13,5 +14,32 @@ window.addEventListener('focus', (event) => {
     audio.play();
 })
 
+audio2.pause();
+
+
+const throne = document.getElementById("throne"),
+    modal = document.querySelector('.my-modal'),
+    body = document.querySelector('body')
+    mang = document.querySelector('.mang'),
+    under = document.querySelector('.under'),
+    rot = document.querySelectorAll('.rot');
+
+throne.addEventListener('click', () => {
+    modal.style.display = "block";
+    body.style.overflow = "hidden";
+    mang.style.display = "block";
+    audio2.play();
+    rot.forEach((item) => {
+        item.classList.add('animation');
+    });
+});
+
+under.addEventListener('click', () => {
+    modal.style.display = "none";
+    body.style.overflow = "visible";
+    rot.forEach((item) => {
+        item.classList.remove('animation');
+    });
+})
 
 
